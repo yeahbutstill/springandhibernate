5a52a76472d993cc489b957768cf541b233797d5
yeahbutstill <mayadani28@gmail.com>
2022-10-06 22:23:48 +0700

    first setup

---
+++ src/main/java/com/yeahbutstill/springandhibernate/SpringandhibernateApplication.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.springandhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringandhibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringandhibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeahbutstill/springandhibernate/SpringandhibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.springandhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringandhibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
