f69571fb932048b56ee7aedceabee7d4b60c99ad
yeahbutstill <mayadani28@gmail.com>
2022-10-07 20:28:19 +0700

    play with spring config

---
+++ src/main/java/com/yeahbutstill/springandhibernate/SwimCoach.java
@@ -0,0 +1,41 @@
+package com.yeahbutstill.springandhibernate;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class SwimCoach implements Coach {
+
+    private FortuneService fortuneService;
+
+    @Value("${foo.email}")
+    private String email;
+
+    @Value("${foo.team}")
+    private String team;
+
+    public SwimCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Swim 1000 meters as a warm uo.";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+
+    public void setFortuneService(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+}
